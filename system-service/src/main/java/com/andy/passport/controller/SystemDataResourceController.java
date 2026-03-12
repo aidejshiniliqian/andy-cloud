e615eb3babd88d19a5a9759968c231e693512c2e
weiliukl <weiliukl@isoftstone>
2026-03-12 17:21:58 +0800

    S120 prompt： 当前项目为基于JDK21版本，目前已开发DataRoleRule角色与数据权限绑定的基础功能，现在需要修改其中的功能：
    1. 在SystemDataResourceController中添加接口/effective/page，写在/page后面，逻辑为：仅查询status为0的数据权限列表
    
    2. 需要参考现有代码格式和风格

--- system-service/src/main/java/com/andy/passport/controller/SystemDataResourceController.java
+++ system-service/src/main/java/com/andy/passport/controller/SystemDataResourceController.java
@@ -45,6 +45,20 @@ public class SystemDataResourceController {
     }
 
     /**
+     * 分页查询有效数据资源表(status=0)
+     * @param page
+     * @param systemDataResourceQueryDto
+     * @return
+     */
+    @GetMapping("/effective/page")
+    @Operation(summary = "分页查询有效数据资源表(status=0)")
+    public CommonResult<IPage<SystemDataResourceVo>> queryPageEffective(Page<SystemDataResource> page,
+                                                               SystemDataResourceQueryDto systemDataResourceQueryDto){
+        IPage<SystemDataResource> systemDataResourceIPage = systemDataResourceService.pageEffective(page, systemDataResourceQueryDto);
+        return CommonResult.success(systemDataResourceIPage.convert(systemDataResource -> BeanUtil.copyProperties(systemDataResource, SystemDataResourceVo.class)));
+    }
+
+    /**
      * 查询单个数据资源
      * @param id
      * @return
